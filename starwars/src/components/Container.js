77f7ede60eff3609a73fe4a555d2e01d940974d9
Luis PM <luisp237@gmail.com>
2020-01-24 14:00:22 -0500

    removed max-width style

--- starwars/src/components/Container.js
+++ starwars/src/components/Container.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 
 const SWParentCont= styled.div`
   display: flex;
-  max-width: 1000px;
+  
   flex-wrap: wrap;
   justify-content: space-around;
   align-items: center;  
